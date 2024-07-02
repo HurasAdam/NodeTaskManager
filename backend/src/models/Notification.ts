286a6cb90d0b2a6460043311bc17dcb4170c8fc3
HurasAdam <huradam@wp.pl>
2024-07-02 22:06:57 +0200

    Feat: Added Notification and Task schema model.

---
+++ backend/src/models/Notification.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const notificationSchema = new mongoose.Schema({
+    team:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}],
+    text:{type:String},
+    task:{type:mongoose.Schema.Types.ObjectId, ref:"Task"},
+    notificationType:{type:String, default:"alert",enum:["alert","message"]},
+    isRead:[{type:mongoose.Types.ObjectId, ref:"User"}],
+},
+{timestamps:true}
+
+)
+
+const Notification = mongoose.model("Notification",notificationSchema;
+    export default Notification;
+)
