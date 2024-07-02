eec660390c9a5d1f0a515ad30a7f8e72dd6014e7
HurasAdam <huradam@wp.pl>
2024-07-02 20:11:04 +0200

    Feat: Added User model with bcrypt password hashing and JWT generation userModel methods.

---
+++ backend/src/models/User.ts
@@ -0,0 +1,43 @@
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+import mongoose from "mongoose";
+
+
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        title: { type: String, required: true },
+        role: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        isAdmin: { type: Boolean, required: true, default: false },
+        tasks: [{ type: mongoose.Schema.Types.ObjectId, ref: "Task" }],
+        isActive: { type: Boolean, required: true, default: true },
+      },
+      { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+    if (this.isDirectModified("password")) {
+        const salt = await bcrypt.genSalt(10);
+      this.password = await bcrypt.hash(this.password, salt);
+      return next();
+    }
+    return next();
+  });
+
+
+userSchema.methods.comparePassword = async function(enteredPassword:string):Promise<boolean>{
+    return await bcrypt.compare(enteredPassword,this.password);
+    };
+
+userSchema.methods.generateJWT = async function(){
+    return await jsonwebtoken.sign({id:this._id},process.env.JWT_SECRET as string,{
+        expiresIn:"30d"
+    })
+}
+
+
+const User = mongoose.model("User",userSchema);
+export default User;
