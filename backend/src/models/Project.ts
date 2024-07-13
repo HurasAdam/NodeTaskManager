99322a01c5ebd0fa3dbba263822fdb67c38d18ce
HurasAdam <huradam@wp.pl>
2024-07-14 01:34:53 +0200

    Feat: Added Project model schema, enhance req.user object with userName field.

--- backend/src/models/Project.ts
+++ backend/src/models/Project.ts
@@ -3,9 +3,21 @@ import * as types from "../types/index";
 import * as enums from "../enums/index";
 
 const projectSchema = new mongoose.Schema({
-    name:{type:String, required:true},
+    name:{type:String, required:true,unique:true},
     pm:{type:mongoose.Schema.Types.ObjectId, ref:"User"},
     members:[{type:mongoose.Schema.Types.ObjectId, ref:"User"}],
+    activities: [
+        {
+          type: {
+            type: String,
+            default: enums.EProjectActivityType.Created,
+            enum:Object.values(enums.EProjectActivityType)
+          },
+          activity: String,
+          date: { type: Date, default: new Date() },
+          by: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        },
+      ],
 })
 
 const Project = mongoose.model("Project",projectSchema);
